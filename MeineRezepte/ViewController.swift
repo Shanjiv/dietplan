9a58d219bd7a47bec84fdca92de9e24bc7c1d9c7
Shanjiv <shan_ratnam@yahoo.de>
2016-05-16 20:20:18 +0200

    implemented methods for the tableview datasource and delegate

--- MeineRezepte/ViewController.swift
+++ MeineRezepte/ViewController.swift
@@ -2,24 +2,71 @@
 //  ViewController.swift
 //  MeineRezepte
 //
-//  Created by Janosan Yoganathan on 15.05.16.
+//  Created by Shanseevan Kanagaratnam on 15.05.16.
 //  Copyright © 2016 Shanseevan Kanagaratnam. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
+    
+    
+    
+    
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    // Tabelledaten
+    var tableData = [String]() {
+        //property observer: only if tableData changes its content
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableData = ["Müsli", "Pizza"]
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    //methods for datasource and delegate protocols
+    
+    //Zeilen
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    
+    //Section
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("rezeptRow")!
+        
+        cell.textLabel?.text = tableData[indexPath.row]
+        
+        return cell
+        
+    }
 
 }
+
 
